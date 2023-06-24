2c48839664c501f11bde3a70a3ad8973d047afee
stivka <kaptenstiv@gmail.com>
2023-06-24 12:30:36 +0300

    add project structure

---
+++ src/main/java/net/stivka/psp/PspApplication.java
@@ -0,0 +1,13 @@
+package net.stivka.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/stivka/psp/PspApplicationTests.java
@@ -0,0 +1,13 @@
+package net.stivka.psp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
