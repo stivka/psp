927a74d91e23836f7eef069e19e36f9a9a539341
stivka <kaptenstiv@gmail.com>
2023-08-19 11:48:34 +0300

    clean up project

--- psp_admin@postgres.session.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM flyway_schema_history;
-
-select * from customer;
-
-select * from merchant;
-
-select * from payment;
-
-select * from api_key;
--- psp_admin@psp.session.sql
+++
@@ -1,5 +0,0 @@
-select * from api_key;
-
-select * from customer;
-
-select * from flyway_schema_history;
